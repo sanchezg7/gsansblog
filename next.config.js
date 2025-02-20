83d6b09e43ec0191377ad318b0a8ee3012fb4c21
sanchezg7 <gsanc.tech@pm.me>
2025-02-19 19:05:35 -0500

    feat: setting up for static page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // SSG
+  assetPrefix: isProd ? '/gsansblog/' : '',
+  basePath: isProd ? '/gsansblog' : '',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
